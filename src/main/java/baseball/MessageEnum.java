e47cda53398bf5caad4a3a851a76f5e68898417e
hoshkush <orrorin@naver.com>
2022-04-17 18:00:50 +0900

    refactor<MessageEnum.java>: change messages
    MessageEnum.java
    - 게임을 종료합니다 -> 게임 종료
    - %d개의 숫자를 모두 맞히셨습니다! -> %d스트라이크 숫자를 모두 맞히셨습니다!

--- src/main/java/baseball/MessageEnum.java
+++ src/main/java/baseball/MessageEnum.java
@@ -18,10 +18,10 @@ public enum MessageEnum {
     PROGRESS_GAME_START("A", "게임을 시작합니다"),
     PROGRESS_INPUT_NUMBER("I", "숫자를 입력해주세요: "),
     PROGRESS_NEW_GAME_OR_QUIT("I", "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요"),
-    PROGRESS_QUIT("Q", "게임을 종료합니다."),
+    PROGRESS_QUIT("Q", "게임 종료"),
 
     RESULT_NOTHING("A", "낫싱"),
-    RESULT_WIN("W", String.format("%d개의 숫자를 모두 맞히셨습니다! 게임 종료", GameProperties.LENGTH_OF_TARGET_NUMBER)),
+    RESULT_WIN("W", String.format("%d스트라이크 숫자를 모두 맞히셨습니다! 게임 종료", GameProperties.LENGTH_OF_TARGET_NUMBER)),
     RESULT_BALL_AND_STRIKE("BS", "%d볼 %d스트라이크"),
     RESULT_ONLY_BALL("B", "%d볼"),
     RESULT_ONLY_STRIKE("S", "%d스트라이크")
